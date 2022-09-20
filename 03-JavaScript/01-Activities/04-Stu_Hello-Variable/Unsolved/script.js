d7e9eb75d59da611623c2235a16913dc3a82fcb4
mdh1080 <110183347+mdh1080@users.noreply.github.com>
2022-09-20 19:47:22 -0400

    Module 3 Assignment 4 complete

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,8 @@
 // Write Your JavaScript Code Here
+var personName = "Megan";
+var pets = "dogs"
+var funFact = "I'm a mom";
+
+console.log("My name is", personName);
+console.log("I have", pets);
+console.log("Fun fact", funFact);
